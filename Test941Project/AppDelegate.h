6a89c66a4485abe54d99afd13021a0918780e2ba
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-11-13 15:27:12 -0800

    initial commit

---
+++ Test941Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test941Project
+//
+//  Created by Benjamin Scholtysik on 11/12/18.
+//  Copyright © 2018 Benjamin Schiltysik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test941Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test941Project
+//
+//  Created by Benjamin Scholtysik on 11/12/18.
+//  Copyright © 2018 Benjamin Schiltysik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
